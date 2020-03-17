ad10aec213effff0393315dc14668c6af0a260bb
erflod5 <2883388711710@ingenieria.usac.edu.gt>
2020-03-17 09:54:59 -0600

    Nueva imagen DB

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const s3 = new AWS.S3(aws_keys.s3);
 const dynDb = new AWS.DynamoDB(aws_keys.dynamodb);
 
 //Routes
-app.post('/api/upload',(req,res)=>{
+app.post('/api/signin',(req,res)=>{
     let body = req.body;
 
     if(body.base64){
@@ -90,14 +90,63 @@ app.post('/api/upload',(req,res)=>{
             }
             else{
                 console.log('Save success:', data);
-                res.send({ 'message': 'ddb success' });
+                res.send({ username : username, src : 'https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940' });
             }
         });
     }
 });
 
-app.post('/api/signin',(req,res)=>{
+app.post('/api/upload',(req,res)=>{
+    let body = req.body;
+    let base64 = body.base64.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/)[2];
+    let extension = body.extension;
+    let username = body.username;
+    
+    //Decoded Image
+    let decodedImage = Buffer.from(base64,'base64');
+    let filename = `${username}-${uuid()}.${extension}`;
 
+    //S3 params 
+    let bucketname = 'bucketfotosg5';
+    let folder = 'fotos/';
+    let filepath = `${folder}${filename}`;
+
+    var uploadParamsS3 = {
+        Bucket: bucketname,
+        Key: filepath,
+        Body: decodedImage,
+        ACL: 'public-read',
+    };
+    s3.upload(uploadParamsS3, function sync(err,data){
+        if (err) {
+            console.log('Error uploading file:', err);
+            res.send({ 'message': 'failed' })
+          } 
+        else {
+            console.log('Upload success at:', data.Location);
+            dynDb.updateItem({
+                TableName : "uPhotos",
+                Key : { "username" : {S: username} },
+                UpdateExpression : "SET #attrName = list_append(#attrName,:attrValue)",
+                ExpressionAttributeNames : {
+                    "#attrName" : "pictures"
+                },
+                ExpressionAttributeValues : {
+                    ":attrValue" : { L : [ { S : data.Location }] }
+                },
+                ReturnValues : "UPDATED_NEW"
+            }, (err,data)=>{
+                if(err){
+                    console.log('Error saving data:', err);
+                    res.send({ 'message': 'ddb failed' });
+                }
+                else{
+                    console.log('Update success:', data);
+                    res.send(data);
+                }
+            });
+        }    
+    });
 });
 
 app.listen(app.get('port'),()=>{
