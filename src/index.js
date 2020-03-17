a49ad98e75557d083f61b075efce3ac87d7a5749
erflod5 <2883388711710@ingenieria.usac.edu.gt>
2020-03-16 18:15:45 -0600

    Upload Image to S3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 //Libs, Modules & Package
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const aws_keys = require('./keys/awsKeys');
+var uuid = require('uuid');
 
 //Inicializaciones
 const app = express();
@@ -10,8 +14,50 @@ app.set('port',process.env.PORT || 3000);
 app.use(cors());
 
 //Midlewares
-app.use(express.json());
-app.use(express.urlencoded({extended :false}));
+app.use(express.json({limit : '5mb'}));
+app.use(express.urlencoded({limit : '5mb',extended :false}));
+
+//AWS
+const s3 = new AWS.S3(aws_keys.s3);
+const dynDb = new AWS.DynamoDB(aws_keys.dynamodb);
+
+//Routes
+app.post('/api/upload',(req,res)=>{
+    let body = req.body;
+
+    let base64 = body.base64;
+    let extension = body.extension;
+    let name = body.name;
+
+    //Decoded Image
+    let decodedImage = Buffer.from(base64,'base64');
+    let filename = `${name}-${uuid()}.${extension}`;
+
+    //S3 params
+    let bucketname = 'bucketfotosg5';
+    let folder = 'usuarios/';
+    let filepath = `${folder}${filename}`;
+    var uploadParamsS3 = {
+        Bucket: bucketname,
+        Key: filepath,
+        Body: decodedImage,
+        ACL: 'public-read',
+    };
+    s3.upload(uploadParamsS3, function sync(err,data){
+        if (err) {
+            console.log('Error uploading file:', err);
+            res.send({ 'message': 'failed' })
+          } 
+        else {
+            console.log('Upload success at:', data.Location);
+            res.send({ 'message': 'uploaded' })
+        }    
+    });
+});
+
+app.post('/api/signin',(req,res)=>{
+
+});
 
 app.listen(app.get('port'),()=>{
     console.log("Servidor corriendo en el puerto " + app.get('port'));
