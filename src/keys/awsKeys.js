18fd98d8090883c7ecfeb03cefa5b5037988bbd9
Erik Flores <41808911+erflod5@users.noreply.github.com>
2020-03-27 13:04:13 -0600

    Update awsKeys.js

--- src/keys/awsKeys.js
+++ src/keys/awsKeys.js
@@ -1,20 +1,19 @@
 let aws_keys = {
     s3: {
-        region: 'us-east-2',
-        accessKeyId: "AKIAYZJK4JNQG5F6LDFC",
-        secretAccessKey: "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",
-        //apiVersion: '2006-03-01',
+        region: '',
+        accessKeyId: "",
+        secretAccessKey: "",
     },
     dynamodb: {
-        apiVersion: '2012-08-10',
-        region: 'us-east-2',
-        accessKeyId: "AKIAYZJK4JNQHXCM3MER",
-        secretAccessKey: "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" 
+        apiVersion: '',
+        region: '',
+        accessKeyId: "",
+        secretAccessKey: "" 
     },
     rekognition: {
-        accessKeyId: "AKIAYZJK4JNQLJU3SLJL",
-        secretAccessKey: "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",
-        region : 'us-east-2'
+        accessKeyId: "",
+        secretAccessKey: "",
+        region : ''
     }
 }
 module.exports = aws_keys;
